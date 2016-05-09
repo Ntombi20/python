b8ff3d292daf3f4896ac07fa288807587ce9e530
Ntombi20 <ntombi@projectcodex.co>
2016-05-09 12:02:45 +0200

    say say

---
+++ date.py
@@ -0,0 +1,6 @@
+import datetime
+
+def hello(name):
+    print "hello " + name , datetime.datetime.now()
+
+hello("ntombi")
---
+++ hello.py
@@ -0,0 +1,9 @@
+
+name = raw_input('What is your name ').lower()
+
+if name == "ntombi":
+    print "DMD is crazy"
+elif name == "mark":
+    print "beauty"
+else:
+    print "I know now python"
